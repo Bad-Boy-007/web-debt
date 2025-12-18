1a00618daf600755f769b7c60b2a584769a55379
mrdgm <mr.dgmironov@mail.ru>
2025-12-19 01:41:22 +0600

    final commity

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <HomeView />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useUserStore } from './stores/userStore'
+import HomeView from './views/HomeView.vue'
+
+const userStore = useUserStore()
+
+onMounted(() => {
+  userStore.loadFromLocalStorage()
+})
+</script>
